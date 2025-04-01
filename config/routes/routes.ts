ca3fca4991ecdb99ba18b6f1d2b44f6ac45ff9fe
Matias Orué C. <matiasorue@hotmail.com>
2025-04-01 10:56:45 -0300

    feat: initialize project with base configuration
    Configured: Husky, eslint, prettier, tailwindcss and react-router

---
+++ config/routes/routes.ts
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from 'react-router';
+import App from '../../src/App';
+
+export const router = createBrowserRouter([{ path: '/', Component: App }]);
---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import react from 'eslint-plugin-react';
+import importPlugin from 'eslint-plugin-import';
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+
+      'no-unused-vars': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+);

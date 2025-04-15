2fa6171fbc4b6ea0e591852743fa2cde40f798a2
Matias Orué C <matiasorue@hotmail.com>
2025-04-15 12:28:35 -0300

    feat: implement porfolio header

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import './App.css';
 import '../config/i18n/i18n';
 import { useTranslation } from 'react-i18next';
-import i18n from '../config/i18n/i18n';
+import Header from './header/header';
 
 function App() {
   const { t } = useTranslation();
 
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng);
-  };
-
   return (
-    <>
-      <button onClick={() => changeLanguage('es')}>es</button>
-      <button onClick={() => changeLanguage('en')}>en</button>
+    <div className='p-5'>
+      <div className='flex flex-col'>
+        <Header />
+      </div>
+
       <div>{t('welcome')}</div>
-    </>
+    </div>
   );
 }
 
